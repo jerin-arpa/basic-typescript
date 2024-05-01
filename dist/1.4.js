7929c21953d09468db59b9b591c03cdc662d9035
Ismat Jerin Arpa <ismatjerinarpa@gmail.com>
2024-05-01 20:25:17 +0600

    Explore Basic data type

---
+++ dist/1.4.js
@@ -0,0 +1,21 @@
+"use strict";
+// Basic DataType
+// String
+let firstName = "Jerin";
+// Number
+let roll = 123;
+// Boolean
+let isAdmin = true;
+// Undefine
+let x = undefined;
+// Null
+let y = null;
+// Anytype
+let d;
+d = 123;
+d = "dnff";
+d = true;
+// Array
+let friends = ["a", "b"];
+friends.push("c");
+let eligibleRollList = [38, 74];
---
+++ src/1.4.ts
@@ -0,0 +1,31 @@
+// Basic DataType
+
+// String
+let firstName: string = "Jerin";
+// Number
+let roll: number = 123;
+// Boolean
+let isAdmin: boolean = true;
+// Undefine
+let x: undefined = undefined;
+// Null
+let y: null = null;
+
+// Anytype
+let d;
+d = 123;
+d = "dnff";
+d = true;
+
+// Array
+let friends: string[] = ["a", "b"];
+friends.push("c");
+console.log(friends);
+
+let eligibleRollList: number[] = [38, 74];
+
+// tuple ---> array --->order --> type of value
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string] = [23, "jerin"];
+let ageNameBool: [number, string, boolean] = [23, "jerin", true];
