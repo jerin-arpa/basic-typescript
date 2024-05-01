7bc94c707ac432e969625682e1dca687f114b20f
Ismat Jerin Arpa <ismatjerinarpa@gmail.com>
2024-05-01 21:12:23 +0600

    Explore Object, Optional and literal type

---
+++ src/1.5.ts
@@ -0,0 +1,14 @@
+// Reference Type ---> Object
+const user: {
+  readonly country: "Bangladesh"; // type--> Literal type
+  firstName: string;
+  middleName?: string; // Optional Type
+  latName: string;
+  isMarried: boolean;
+} = {
+  country: "Bangladesh",
+  firstName: "Ismat",
+  middleName: "Jerin",
+  latName: "Arpa",
+  isMarried: true,
+};
